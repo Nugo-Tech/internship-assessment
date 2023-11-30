e965318ee4af6ebbbf39181b83e708bab64f3caf
Sajana <sajanapeshitha@gmail.com>
2023-11-30 16:33:36 +0530

    Added pagination

--- src/features/characterSlice.js
+++ src/features/characterSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     characters: [],
     pageNumber: 1,
+    totalPages: 1,
 }
 
 const characterSlice = createSlice({
@@ -10,10 +11,15 @@ const characterSlice = createSlice({
     initialState,
     reducers: {
         setCharacters(state, action) {
-            state.characters = action.payload;
+            state.characters = action.payload.results;
+            state.totalPages = action.payload.info.pages;
+        },
+
+        setPage(state, action) {
+            state.pageNumber = action.payload;
         }
     }
 })
 
-export const { setCharacters } = characterSlice.actions;
+export const { setCharacters, setPage } = characterSlice.actions;
 export default characterSlice.reducer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,61 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getCharacters } from "../services/ApiService";
-import { setCharacters } from "../features/characterSlice";
+import { setCharacters, setPage } from "../features/characterSlice";
 import CharacterCard from "../components/CharacterCard";
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Pagination from "@mui/material/Pagination";
 
 const Home = () => {
     const dispatch = useDispatch();
     const pageNumber = useSelector(state => state.character.pageNumber);
     const characters = useSelector(state => state.character.characters);
+    const totalPages = useSelector(state => state.character.totalPages);
 
     useEffect(() => {
         const fetchCharacters = async () => {
             const response = await getCharacters(pageNumber);
-            dispatch(setCharacters(response.results));
+            dispatch(setCharacters(response));
         }
         fetchCharacters();
     }, [pageNumber, dispatch]);
 
+    const handlePageSelect = (event, value) => {
+        dispatch(setPage(value))
+    }
+
     return (
-        <Container maxWidth="lg">
-            <h1 className="title">Rick & Morty Characters</h1>
-            <Grid container spacing={2}>
-                {characters.length > 0 ? (
-                    characters.map((character) => {
-                        return (
-                            <Grid item xs={12} md={6} lg={4} key={character.id}>
-                                <CharacterCard character={character} />
-                            </Grid>
-                        )
-                    })
-                ) : (
-                    <div>
-                        <p>No characters</p>
-                    </div>
-                )}
-            </Grid>
-        </Container>
+        <div>
+            <Container maxWidth="lg" >
+                <h1 className="title">Rick & Morty Characters</h1>
+                <Grid container spacing={5}>
+                    {characters.length > 0 ? (
+                        characters.map((character) => {
+                            return (
+                                <Grid item xs={12} md={6} lg={4} key={character.id}>
+                                    <CharacterCard character={character} />
+                                </Grid>
+                            )
+                        })
+                    ) : (
+                        <div>
+                            <p>No characters</p>
+                        </div>
+                    )}
+                </Grid>
+            </Container>
+
+            <div className="pagination-container">
+                <Pagination
+                    count={totalPages}
+                    boundaryCount={3}
+                    siblingCount={2}
+                    color="primary"
+                    size="large"
+                    sx={{ p: 2 }} onChange={handlePageSelect} />
+            </div>
+        </div>
     )
 }
 
