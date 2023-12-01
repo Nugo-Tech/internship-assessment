df69513089e1226c417ffa871feb8e7dbc32b1ad
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-12-02 00:13:57 +0530

    Created thunk to fetch data

--- src/features/characterSlice.js
+++ src/features/characterSlice.js
@@ -1,30 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const API_URL = 'https://rickandmortyapi.com/api/character/';
 
-const fetchAllCharacters = async() => {
-    try {
-        const {data, status} = await axios.get(API_URL);
-        if(status === 200){
-            return data.results;
+export const fetchAllCharacters = createAsyncThunk(
+    'characters/fetchAll',
+    async() => {
+        try {
+            const {data, status} = await axios.get(API_URL);
+            if(status === 200){
+                return data.results;
+            }
+        } catch(err) {
+            console.error(err);
         }
-    } catch(err) {
-        console.error(err);
     }
-}
-fetchAllCharacters();
+);
 
 const initialState = {
     characterData: [],
-}
+};
 
 export const characterSlice = createSlice({
     name: 'character',
     initialState,
     reducers: {
-
-    }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchAllCharacters.fulfilled, (state, { payload }) => {
+                state.characterData = payload;
+            });
+    },
 });
 
 export default characterSlice.reducer;
