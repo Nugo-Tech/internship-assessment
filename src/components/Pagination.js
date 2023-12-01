4b9f424033e3c475ec044cdb783eb1bc8ebb20c1
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-12-02 05:21:21 +0530

    Pagination component implemented

---
+++ src/components/Pagination.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAllCharacters } from '../features/characterSlice';
+
+const Pagination = () => {
+    const currentPage = useSelector(state => state.character.currentPage);
+    const pageNumbersArray = useSelector(state => state.character.pagesArray);
+    const dispatch = useDispatch();
+
+    const handlePageChange = (number) => {
+        if(number !== '..') {
+            dispatch(fetchAllCharacters(number));
+        }
+    }
+
+    useEffect(() => {
+        handlePageChange(currentPage);
+    }, [currentPage]);
+
+    return (
+        <div className="flex flex-col items-center">
+            <div className='flex h-12 font-medium rounded-full gap-4'>
+                {pageNumbersArray.map((pageNumber) => (
+                    <div 
+                        onClick={() => handlePageChange(pageNumber)}
+                        className={`w-12 flex justify-center items-center cursor-pointer 
+                            leading-5 transition duration-150 ease-in  rounded-full 
+                            ${currentPage === pageNumber ? 'bg-slate-600 text-white' : ''}  
+                            ${(pageNumber !== '..' && pageNumber !== currentPage )? 'hover:bg-slate-400 hover:text-white' : ''}`}
+                    >
+                        {pageNumber}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Pagination;
