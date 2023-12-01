9df6a9ed5a32b64eda3b2b4087289f7bd2baff8d
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-12-01 18:54:09 +0530

    API call with axios

--- src/features/characterSlice.js
+++ src/features/characterSlice.js
@@ -1,4 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const API_URL = 'https://rickandmortyapi.com/api/character/';
+
+const fetchAllCharacters = async() => {
+    try {
+        const {data, status} = await axios.get(API_URL);
+        if(status === 200){
+            return data.results;
+        }
+    } catch(err) {
+        console.error(err);
+    }
+}
+fetchAllCharacters();
 
 const initialState = {
     characterData: [],
